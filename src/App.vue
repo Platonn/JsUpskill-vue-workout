b7218f6dc63d98b0201dd2b0d5337d372c89b817
vagabondi <kamil.mateusiak.gmail.com>
2017-04-11 18:46:15 +0200

    2

--- src/App.vue
+++ src/App.vue
@@ -1,1 +1,38 @@
-<template></template>
+<template>
+  <div class="wrapper">
+      <div class="containter">
+        <trainings></trainings>
+      </div>
+  </div>
+</template>
+
+<script>
+import Trainings from './components/Trainings.vue'
+
+ export default {
+   data () {
+     return {
+      message: "Hello",
+      fruits: [
+        'Apple',
+        'Banana'
+      ] 
+     } 
+   },
+   methods: {
+     showMsg (msg) {
+       console.log(msg)
+     }
+   },
+   components: {
+    trainings: Trainings
+   }
+ }  
+</script>
+
+<style>
+  .wrapper {
+    background-color: #ffd700;
+    overflow: hidden;
+  }
+</style>
---
+++ src/components/Trainings.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="col-xs-12 col-sm-8 col-sm-offset-2">
+    <div class="col-xs-6">
+      <div class="panel panel-primary">
+        <div class="panel-heading">nazwa</div>
+        <div class="panel-body">
+          <p>opis</p>
+          <button class="btn btn-primary center-block">Wybierz</button>
+        </div>
+      </div>
+    </div>
+    <div class="col-xs-6">
+      <div class="panel panel-primary">
+        <div class="panel-heading">nazwa</div>
+        <div class="panel-body">
+          <p>opis</p>
+          <button class="btn btn-primary center-block">Wybierz</button>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
